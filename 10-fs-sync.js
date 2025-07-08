2c1844d753482234b588dd8ae7b22db91281dcc1
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-07-09 00:12:37 +0200

    comandos npm

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,11 +1,8 @@
 const {readFileSync, writeFileSync} = require('fs');
-
+console.log('start')
 //Para leer el contenido de un archivo
 const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
-
-console.log(first)
-console.log(second)
 
 //Crear y escribir un nuevo archivo
 writeFileSync(
@@ -13,3 +10,6 @@ writeFileSync(
     `Here is the result: ${first}, ${second}`,
     {flag: 'a'} //Escribir por encima de lo que ya hay
 )
+
+console.log('Done with the task')
+console.log('Starting the next one')
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const {readFileSync, writeFileSync, readFile} = require('fs');
+//Comandos NPM
+//npm (Comando global)
+//npm --version (Muestra la version de npm)
 
-readFile('./content/first.txt', (err, result)=> {
-    if(err){
-        console.log(err)
-        return null
-    }else{
-        console.log(result)
-    }
-})
+
+//Podemos instalar paquetes como dependencia local, es decir en un proyecto en particular 
+//npm i <nombreDelPaquete>
+
+//O Podemos instalar paquetes como dependencia global, es decir que lo podamos utilizar en cualquier proyecto
+//npm install -g <nombreDelPaquete>
